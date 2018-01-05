13e3eb3af66b2b9a67f064ce35255a584721032f
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-05 15:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function logDriversByHometown(drivers, location) {
 }
 
 function driversByRevenue(drivers) {
-  sortedDrivers = [...drivers]
-  sortedDrivers = sortedDrivers.sort(function (a, b) {
+  sortedDrivers = [...drivers].sort(function (a, b) {
     return a.revenue - b.revenue
   });
 
